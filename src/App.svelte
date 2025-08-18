404fb56ff16f89d0e34755d6e06ea68aa10203cf
Sebastien CHAUVEL <monsieurchauvel22@gmail.com>
2025-08-18 13:55:42 +0200

    Creation du wireframe + mise en place du projet (font, icones, markdown css)

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,11 @@
 <script>
 </script>
 
-<p>Bienvenue en saison A07 ! (Enlève ce paragraphe pour écrire ton code HTML à la place 😉)</p>
+<aside>
 
+</aside>
+<section>
+    
+</section>
 <style>
 </style>
